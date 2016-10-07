efe3bf45b74e63e3295e35c03db8fc0437562d85
Andrew Dillion-Walshe <andrewdillion6@gmail.com>
2016-10-07 15:12:33 -0400

    force all http requests to use https

--- server.js
+++ server.js
@@ -11,9 +11,20 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
+// set all incoming requests to use https
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https')
+    return res.redirect([
+      'https://',
+      req.get('Host'),
+      req.url
+    ].join(''));
+  else
+    next() /* Continue to other routes if we're not redirecting */
+})
+
 // set the home page route
 app.get('/', function(req, res) {
-
   // ejs render automatically looks in the views folder
   res.render('index');
 });
